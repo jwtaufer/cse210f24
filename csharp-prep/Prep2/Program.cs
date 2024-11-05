05012c244f8e39c37340ec80940e6eaff2216599
jwtaufer <jwtaufer@gmail.com>
2024-11-05 12:53:25 -0700

    Completed Prep2 Core Requirement 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+
+        int percentage = int.Parse(userInput);
+
+        if (percentage >= 90)
+        {
+            Console.WriteLine("Grade: A");
+        }
+        else if (percentage >= 80)
+        {
+            Console.WriteLine("Grade: B");
+        }
+        else if (percentage >= 70)
+        {
+            Console.WriteLine("Grade: C");
+        }
+        else if (percentage >= 60)
+        {
+            Console.WriteLine("Grade: D");
+        }
+        else if (percentage < 60)
+        {
+            Console.WriteLine("Grade: F");
+        }
     }
 }
