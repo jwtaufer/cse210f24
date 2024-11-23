de82b194a6b7e2adacfe09deff80323c63376b72
jwtaufer <jwtaufer@gmail.com>
2024-11-23 14:25:38 -0700

    Completed Encapsulation - Design Activity step 11

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,25 @@
+class Scripture
+{
+    private Reference _reference = new Reference();
+    private List<Word> _words = new Word();
+
+    public Scripture(Reference Reference, string text)
+    {
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        
+    }
+}
