5827e6093ffcc404294bffd662098be11d9818a4
jwtaufer <jwtaufer@gmail.com>
2024-12-04 13:04:13 -0700

    Completed Learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment test1 = new Assignment("Jake", "English");
+        Console.WriteLine(test1.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment test2 = new MathAssignment("Lis", "Multiplication", "3", "4-20");
+        Console.WriteLine(test2.GetSummary());
+        Console.WriteLine(test2.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment test3 = new WritingAssignment("Kip Hartvigsen", "English", "Writing is hell");
+        Console.WriteLine(test3.GetSummary());
+        Console.WriteLine(test3.GetWritingInformation());
     }
 }
