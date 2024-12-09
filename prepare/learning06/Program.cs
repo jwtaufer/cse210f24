711b810588878668ea039cd1ee86b2efce886c0f
jwtaufer <jwtaufer@gmail.com>
2024-12-09 14:47:54 -0700

    Completed Learning06

--- prepare/learning06/Program.cs
+++ prepare/learning06/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Shape square = new Square("red", 5);
+        shapes.Add(square);
+
+        Shape rectangle = new Rectangle("yellow", 5, 7);
+        shapes.Add(rectangle);
+
+        Shape circle = new Circle("blue", 2);
+        shapes.Add(circle);
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine($"The area of this {shape.GetColor()} {shape} is {shape.GetArea()} units squared.");
+        }
     }
 }
